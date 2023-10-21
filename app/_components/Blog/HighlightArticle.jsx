5c1128b455396db7c9a3649ee5b2e124421b10aa
Niklas <niklas@kizo-agency.com>
2023-10-21 12:04:44 +0200

    Highlight Article on Blog page jsx structure

---
+++ app/_components/Blog/HighlightArticle.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const HighlightArticle = ({ data }) => {
+  const { headline, excerpt, slug, featuredImage } = data;
+  return (
+    <article className="highlight-article">
+      <div className="highlight-article__info">
+        <h3>{headline}</h3>
+        <p className="copy">{excerpt}</p>
+        <Link className="btn btn--turquoise btn--medium" href={`/blog/${slug}`}>
+          Read more
+        </Link>
+      </div>
+      <img src={featuredImage} alt="" />
+    </article>
+  );
+};
+
+export default HighlightArticle;
---
+++ app/blog/page.jsx
@@ -0,0 +1,24 @@
+import HighlightArticle from "../_components/Blog/HighlightArticle";
+
+export default function Page() {
+  const highlightArticleData = {
+    headline: "3 tips for a super fast takeoff",
+    excerpt: (
+      <>
+        Improving your take-off phase in surfing is a fundamental step toward
+        riding waves with more confidence and style. Improving your take-off
+        phase is a gradual process, and it may take time to master. Be patient,
+        stay committed to practice, and enjoy the journey of becoming a better
+        surfer. With dedication and persistence, you'll see progress and have
+        more enjoyable rides. Here is how:
+      </>
+    ),
+    slug: "takeoff",
+    featuredImage: "/assets/hero-experience.png",
+  };
+  return (
+    <main>
+      <HighlightArticle data={highlightArticleData} />
+    </main>
+  );
+}
