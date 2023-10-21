bee46a18eb3145b3358d87f9c8d98f8ce5b38b8e
Niklas <niklas@kizo-agency.com>
2023-10-21 12:57:25 +0200

    Newsletter subscriber functionality

--- app/_components/Blog/SubscribeToNewsletter.jsx
+++ app/_components/Blog/SubscribeToNewsletter.jsx
@@ -1,25 +1,54 @@
 "use client";
+import { useState } from "react";
 
 const SubscribeToNewsletter = () => {
+  const [email, setEmail] = useState("");
+  const [hasSignedUp, setHasSignedUp] = useState(false);
+
+  const onChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (email.length) {
+      // Send Email to Strapi
+      setHasSignedUp(true);
+    }
+  };
+
   return (
     <section className="newsletter">
-      <div className="newsletter__info">
-        <h4>subscribe to our newsletter</h4>
-        <p className="copy">
-          Unlock Exclusive Insights and Stay In the Know – Subscribe to Our
-          Newsletter Today to always stay in touch
-        </p>
-      </div>
-      <form className="newsletter__form">
-        <input
-          type="text"
-          className="newsletter__email input"
-          placeholder="Enter your E-mail address"
-        />
-        <button className="newsletter__subscribe btn btn--medium btn--turquoise">
-          SUBSCRIBE
-        </button>
-      </form>
+      {hasSignedUp ? (
+        <h4 className="newsletter__thanks">
+          Thank you for signing up for our newsletter
+        </h4>
+      ) : (
+        <>
+          <div className="newsletter__info">
+            <h4>subscribe to our newsletter</h4>
+            <p className="copy">
+              Unlock Exclusive Insights and Stay In the Know – Subscribe to Our
+              Newsletter Today to always stay in touch
+            </p>
+          </div>
+          <form className="newsletter__form" onSubmit={onSubmit}>
+            <input
+              type="text"
+              className="newsletter__email input"
+              placeholder="Enter your E-mail address"
+              value={email}
+              onChange={onChange}
+            />
+            <button
+              type="submit"
+              className="newsletter__subscribe btn btn--medium btn--turquoise"
+            >
+              SUBSCRIBE
+            </button>
+          </form>
+        </>
+      )}
     </section>
   );
 };
