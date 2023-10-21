e0187040f2ea38c6f6e28f11eea57f1bccb0b5ef
Niklas <niklas@kizo-agency.com>
2023-10-21 19:52:36 +0200

    Fetch data on individual blog page

---
+++ app/blog/[article]/page.jsx
@@ -0,0 +1,22 @@
+import { fetchBlogArticles, fetchDataFromStrapi } from "@/utils/strapi.utils";
+
+export default async function Page({ params }) {
+  const { article: slug } = params;
+
+  const articles = await fetchBlogArticles();
+  const article = articles.find((article) => article.slug === slug);
+
+  return (
+    <main>
+      <h1>{article.headline}</h1>
+    </main>
+  );
+}
+
+export async function generateStaticParams() {
+  const articles = await fetchDataFromStrapi("blog-articles");
+
+  return articles.map((article) => ({
+    article: article.attributes.slug,
+  }));
+}
