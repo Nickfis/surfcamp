a18432fb9ada08fca9d19cb8ad1fdb9465a40751
Niklas <niklas@kizo-agency.com>
2023-10-22 18:31:20 +0200

    BlogPreview Component Styling

--- app/_components/BlogPreview/BlogPreviewItem.jsx
+++ app/_components/BlogPreview/BlogPreviewItem.jsx
@@ -1,14 +1,15 @@
 import { formatDate } from "@/utils/strapi.utils";
+import Link from "next/link";
 
 const BlogPreviewItem = ({ article }) => {
   return (
-    <div className="blog-preview__item">
+    <Link href={`/blog/${article.slug}`} className="blog-preview__item">
       <div className="blog-preview__image">
         <img src={article.featuredImage} alt="" />
       </div>
-      <h5 className="blog-preview__headline">{article.headline}</h5>
+      <h5 className="blog-preview__title">{article.headline}</h5>
       <p className="copy-small">{formatDate(article.publishedAt)}</p>
-    </div>
+    </Link>
   );
 };
 
